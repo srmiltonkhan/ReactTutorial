1506b5a753e78e8400371842f1a62c8fb4df9204
Milton Khan <43887779+srmiltonkhan@users.noreply.github.com>
2022-03-20 09:35:00 +0600

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Todo from "./component/Todo";
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <>
+        <Todo />
+      </>
+    );
+  }
+}
---
+++ component/Todo.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+function Todo() {
+    const [todo, setTodo] = useState("");
+    const [warning, setWarning] = useState("");
+
+    const handleInput = (e) =>{
+        const inputValue = e.target.value;
+        const updatingWarning = inputValue.includes('.js')?'You need Javascript': null;
+
+
+        setTodo(inputValue)
+        setWarning(updatingWarning)
+    }
+
+    return (  
+        <>
+            <h1>{todo}</h1>
+            <p>
+                <textarea value={todo} onChange = {handleInput}/>
+            </p>
+            <hr />
+
+            <p>{warning || 'Good Choice'}</p>
+        </>
+    );
+}
+
+export default Todo;
